b12832dd444e69f75154c406e3c2ad7bff612ec5
lancy <lancy1014@gmail.com>
2013-06-10 00:43:41 +0800

    Initial Commit

---
+++ buddy/GLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GLAppDelegate.h
+//  buddy
+//
+//  Created by Lancy on 10/6/13.
+//  Copyright (c) 2013 GraceLancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
