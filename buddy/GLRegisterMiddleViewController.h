b0ed57e94872218148dbac76a61c4dc6dc32cdc6
lancy <lancy1014@gmail.com>
2013-08-04 17:23:52 +0800

    ADD: add register view controller

---
+++ buddy/GLRegisterMiddleViewController.h
@@ -0,0 +1,15 @@
+//
+//  GLRegisterMiddleViewController.h
+//  buddy
+//
+//  Created by Lancy on 4/8/13.
+//  Copyright (c) 2013 GraceLancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLRegisterMiddleViewController : UIViewController
+
+@property (assign, nonatomic, getter = isChooseOld) BOOL chooseOld;
+
+@end
---
+++ buddy/GLRegisterMiddleViewController.m
@@ -0,0 +1,48 @@
+//
+//  GLRegisterMiddleViewController.m
+//  buddy
+//
+//  Created by Lancy on 4/8/13.
+//  Copyright (c) 2013 GraceLancy. All rights reserved.
+//
+
+#import "GLRegisterMiddleViewController.h"
+
+@interface GLRegisterMiddleViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *avatarBackgroundView;
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *subtitleLabel;
+@end
+
+@implementation GLRegisterMiddleViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setupUserInterface];
+}
+
+- (void)setupUserInterface
+{
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"viewcontroller_bg"]]];
+    [self setTitle:@"注册"];
+    
+    if ([self isChooseOld]) {
+        [self.avatarBackgroundView setImage:[UIImage imageNamed:@"oldframe.png"]];
+        [self.titleLabel setText:@"您选择了伴儿老人端"];
+        [self.subtitleLabel setText:@"老人端提供全套语音导航，帮助老人快速拨通亲人电话，接收语音提醒，传达思念"];
+    }
+}
+
+- (IBAction)didTapNextButton:(id)sender {
+    [self performSegueWithIdentifier:@"next" sender:self];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
